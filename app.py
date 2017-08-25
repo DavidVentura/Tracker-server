6ef5f2022b8e0da1d9b1987a6769e3251dd8f72b
David <davidventura27@gmail.com>
2017-08-25 17:25:30 -0300

    filter points based on distance for linestring

--- app.py
+++ app.py
@@ -34,17 +34,12 @@ def main():
 
 @app.route('/track/<string:phone_id>')
 def track(phone_id):
-    print(phone_id)
-    yesterday = datetime.datetime.today() - datetime.timedelta(seconds=6*3600)
-    print(yesterday)
-    res = coll.find({'ID': phone_id,
-                     'tstamp': {'$gte': yesterday}},
-                    {'location.LAT': 1, 'location.LONG': 1, 'tstamp': 1, '_id': 0})
-    res.sort('tstamp', -1)
-    ret = list(map(fixts, res))
-    linestring = list(map(lambda x: [x['location']['LONG'], x['location']['LAT']], ret))
+    six_hours = datetime.datetime.today() - datetime.timedelta(seconds=6*3600)
+    ret = track(phone_id, six_hours)
+    linestring = data_to_linestring(ret)
+    filtered = filter_close(linestring, 0.0000003)
 
-    return jsonify({'ls': linestring, 'ret':ret})
+    return jsonify({'ls': filtered, 'ret':ret})
 
 
 @app.route('/state/<string:phone_id>')
@@ -63,5 +58,39 @@ def fixts(item):
     return item
 
 
+def track(phone_id, since):
+    res = coll.find({'ID': phone_id,
+                     'tstamp': {'$gte': since}},
+                    {'location.LAT': 1, 'location.LONG': 1, 'tstamp': 1, '_id': 0})
+    res.sort('tstamp', -1)
+    ret = list(map(fixts, res))
+    return ret
+
+
+def distance(p, q):
+    d = (p[0]-q[0])**2 + (p[1]-q[1])**2
+    return d
+
+
+def filter_close(points, dst):
+    ret = [points[0]]
+    for idx in range(len(points) - 1):
+        if distance(ret[-1], points[idx]) > dst:
+            ret.append(points[idx])
+    return ret
+
+
+def data_to_linestring(data):
+    return list(map(lambda x: [x['location']['LAT'], x['location']['LONG']], data))
+
+
 if __name__ == '__main__':
+    # six_hours = datetime.datetime.today() - datetime.timedelta(seconds=6*3600)
+    # l = track("", six_hours)
+    # l = track("", six_hours)
+    # data = data_to_linestring(l)
+    # print(len(data))
+    # filtered = filter_close(data, 0.0000003)
+    # print(filtered)
+    # print(len(filtered))
     app.run(host="0.0.0.0", port=4567, debug=True)
